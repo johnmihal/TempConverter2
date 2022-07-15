aa3a7a1ad8f341ca677228011109b8af2c004b1d
John Mihal <107288799+johnmihal@users.noreply.github.com>
2022-07-15 15:04:47 -0700

    Initial Commit

---
+++ TempConverter2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TempConverter2
+//
+//  Created by John Mihal on 7/15/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TempConverter2/TempConverter2App.swift
@@ -0,0 +1,17 @@
+//
+//  TempConverter2App.swift
+//  TempConverter2
+//
+//  Created by John Mihal on 7/15/22.
+//
+
+import SwiftUI
+
+@main
+struct TempConverter2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
